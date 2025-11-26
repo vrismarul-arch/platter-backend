8dda6a3beca1ed24b482d9b2aab99f35ff278fe5
vrismarul-arch <vrismarul@gmail.com>
2025-11-26 21:38:26 +0530

    mport 5004

--- server.js
+++ server.js
@@ -53,5 +53,5 @@ app.use("/api/auth", authRoutes);
 app.get("/", (req, res) => res.send("✅ API is running..."));
 
 // Start server
-const PORT = process.env.PORT || 5003;
+const PORT = process.env.PORT || 5004;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
